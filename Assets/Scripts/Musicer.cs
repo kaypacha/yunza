77b2f527ccb313261bda374f88439e6c88430041
Chorri <josemariacosta_8@hotmail.com>
2020-10-11 15:07:23 -0500

    Added a way to increase slowly the volume of the music

---
+++ Assets/Scripts/Musicer.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Musicer : MonoBehaviour
+{
+
+    public AudioSource source;
+    public float targetVolume;
+    float inter = 0;
+    public float changeSpeed;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (inter < 1)
+        {
+            inter += changeSpeed * Time.deltaTime;
+        } else
+        {
+            inter = 1;
+        }
+
+        source.volume = Mathf.Lerp(0,targetVolume,inter);
+    }
+}
