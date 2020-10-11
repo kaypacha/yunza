910debac5a9c6cf164f0fd86da9085e5476e6476
Chorri <josemariacosta_8@hotmail.com>
2020-10-10 23:26:54 -0500

    Added waiter just in case, useless for now

--- Assets/Scripts/Waiter.cs
+++ Assets/Scripts/Waiter.cs
@@ -1,9 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using UnityEngine;
 
 public class Waiter : MonoBehaviour
 {
+    public float timer;
+    public CamMove cM;
+    public CamHoverEffect cHE;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +18,10 @@ public class Waiter : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Time.time > timer)
+        {
+            cM.enabled = true;
+            cHE.enabled = true;
+        }
     }
 }
