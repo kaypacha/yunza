23cef7733da17530a8fc3ae7faabef68f94e60b2
Chorri <josemariacosta_8@hotmail.com>
2020-10-11 11:57:25 -0500

    Added Splinter Prefab and behaviour

---
+++ Assets/Scripts/Axe/Splinterer.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Splinterer : MonoBehaviour
+{
+
+    Rigidbody2D rb;
+    public float minX;
+    public float maxX;
+    public float minY;
+    public float maxY;
+
+    public float multForce;
+    float timeStart;
+    public float lifeTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+
+        float nX = Random.Range(minX,maxX);
+        float nY = Random.Range(minY, maxY);
+        Vector2 force = new Vector2(nX*multForce, nY * multForce);
+
+        timeStart = Time.time;
+
+        rb.velocity = force;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time - timeStart > lifeTime)
+        {
+            Destroy(this);
+        }
+    }
+}
