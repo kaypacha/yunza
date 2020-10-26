f1c4dc4fd1bda8a4cb4a120bffde29afa728128f
Chorri <josemariacosta_8@hotmail.com>
2020-10-26 14:38:39 -0500

    Added GameStarter Script for the Initial Wait

---
+++ Assets/Scripts/GameStarter.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameStarter : MonoBehaviour
+{
+
+    public float timer;
+    float timestart;
+
+    public Text counter;
+    public GameObject toActivate;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timestart = Time.time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int value = (int)(timestart + timer - Time.time);
+        counter.text = value.ToString();
+
+        if (timestart + timer - Time.time <= 0)
+        {
+            toActivate.SetActive(true);
+            gameObject.SetActive(false);
+        }
+    }
+}
--- Assets/Scripts/Waiter.cs
+++ Assets/Scripts/Waiter.cs
@@ -11,10 +11,16 @@ public class Waiter : MonoBehaviour
     public CamHoverEffect cHE;
     public CamZoomer cZ;
 
+    public List<ZoneEntity> zones;
+
     // Start is called before the first frame update
     void Start()
     {
         currentTime = Time.time;
+        foreach (ZoneEntity item in zones)
+        {
+            item.enabled = true;
+        }
     }
 
     // Update is called once per frame
